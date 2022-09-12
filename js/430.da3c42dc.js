e1c95e64408e2e717b4f5706de95189df21029bc
jovinhz <jovinharris@gmail.com>
2022-09-12 22:59:27 +0700

    update dist

--- js/430.da3c42dc.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self["webpackChunksumatera_soil"]=self["webpackChunksumatera_soil"]||[]).push([[430],{5423:function(a,r,e){e.r(r),e.d(r,{default:function(){return d}});var n=e(3396);function s(a,r,e,s,u,t){return(0,n.wg)(),(0,n.iD)("div",null,"dashboard")}var u=(0,n.aZ)({name:"Dashboard",methods:{}}),t=e(89);const o=(0,t.Z)(u,[["render",s]]);var d=o}}]);
